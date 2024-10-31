58731cd1b27a70d219f8b2cbdcd01646e922e81c
1awp1 <loktevdiman@mail.ru>
2024-10-31 15:34:05 +0400

    добавление страниц в проект  LAB4-3

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin'); // Добавьте CopyWebpackPlugin 
 
 module.exports = {
   entry: './src/index.js', 
@@ -13,16 +14,65 @@ module.exports = {
         test: /\.css$/, 
         use: ['style-loader', 'css-loader'], 
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/, // Добавьте правило для обработки картинок
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]', // Имя файла будет сохранено, как есть
+              outputPath: 'images/', // Картинки будут копироваться в папку 'images'
+            },
+          },
     ],
+  }
+]
   },
-  plugins: [
+  plugins: [   
     new HtmlWebpackPlugin({
       template: './src/index.html',
       inject: true,
       chunks: ['index'],
-      filename: 'index.html'
+      filename: 'index.html',
     }),
+    new HtmlWebpackPlugin({
+      template: './src/projects.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'projects.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/task.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'task.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/about.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'about.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/add-task.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'add-task.html',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './src/images', to: 'cat404' },
+        { from: './src/images', to: 'images1' },
+        { from: './src/images', to: 'Start' }
+      ]
+    })
   ],
+
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
